5d3702ad1a3603b6586d9efe0d00dcf3e3256425
Christian Brintnall <christianbrintnall@gmail.com>
2023-01-05 09:08:59 -0800

    Add tier system, setup tiers to unlock previous tiers

--- ForgivingPortalsManager.cs
+++ ForgivingPortalsManager.cs
@@ -1,39 +1,16 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using System.Collections.Generic;
-using UnityEngine;
+using ForgivingPortals.Tiers;
 
 namespace ForgivingPortals
 {
-  [BepInPlugin("com.otter.bee.portals", "Forgiving Portals", "0.0.1")]
+  [BepInPlugin("com.otter.bee.portals", "Forgiving Portals", "1.1.0")]
   public class ForgivingPortalsManager : BaseUnityPlugin
   {
     public static BepInEx.Logging.ManualLogSource Log;
 
-    public static bool TinCopperTeleportUnlocked = false;
-    public static bool IronTeleportUnlocked = false;
-    public static bool SilverTeleportUnlocked = false;
-
-    public static HashSet<string> TinCopperItems = new HashSet<string>()
-    {
-      "$item_copper",
-      "$item_tin",
-      "$item_bronze",
-      "$item_tinore",
-      "$item_copperore"
-    };
-
-    public static HashSet<string> IronItems = new HashSet<string>()
-    {
-      "$item_iron",
-      "$item_ironscrap"
-    };
-
-    public static HashSet<string> SilverItems = new HashSet<string>()
-    {
-      "$item_silver",
-      "$item_silverore"
-    };
+    public static List<Tier> Tiers;
 
     private Harmony _harmony;
 
@@ -46,27 +23,40 @@ namespace ForgivingPortals
       _harmony = new Harmony("com.otter.bee.portals");
       _harmony.PatchAll();
 
+      Logger.LogInfo("Creating tiers...");
+
+      Tier tinCopper = new Tier(
+        new List<string>() { "$item_copper", "$item_tin", "$item_bronze", "$item_tinore", "$item_copperore" },
+        new List<string>() { "$item_iron" }
+      );
+
+      Tier iron = new Tier(
+        new List<string>() { "$item_iron", "$item_ironscrap" },
+        new List<string>() { "$item_silver" },
+        tinCopper
+      );
+
+      Tier silver = new Tier(
+        new List<string>() { "$item_silver", "$item_silverore" },
+        new List<string>() { "$item_blackmetal" },
+        iron
+      );
+
+      Tiers = new List<Tier>()
+      {
+        tinCopper,
+        iron,
+        silver
+      };
+
       Logger.LogInfo("Done");
     }
 
     public static void SyncTeleportationState(List<ItemDrop.ItemData> items)
     {
-      foreach(var item in items)
+      foreach(Tier tier in Tiers)
       {
-        if (TinCopperTeleportUnlocked && TinCopperItems.Contains(item.m_shared.m_name))
-        {
-          item.m_shared.m_teleportable = true;
-        }
-
-        if (IronTeleportUnlocked && IronItems.Contains(item.m_shared.m_name))
-        {
-          item.m_shared.m_teleportable = true;
-        }
-
-        if (SilverTeleportUnlocked && SilverItems.Contains(item.m_shared.m_name))
-        {
-          item.m_shared.m_teleportable = true;
-        }
+        tier.SyncItems(items);
       }
     }
 
@@ -75,33 +65,12 @@ namespace ForgivingPortals
     /// tier.
     /// </summary>
     /// <param name="item">The item to be examined</param>
-    /// <returns>If anything was unlocked</returns>
-    public static bool CheckForUnlocks(ItemDrop.ItemData item)
+    public static void CheckForUnlocks(ItemDrop.ItemData item)
     {
-      string name = item.m_shared.m_name;
-
-      if (!TinCopperTeleportUnlocked && name == "$item_iron")
+      foreach(Tier tier in Tiers)
       {
-        TinCopperTeleportUnlocked = true;
-        Log.LogInfo("Unlocked tin / copper / bronze teleporting.");
-        return true;
+        tier.CheckUnlock(item);
       }
-
-      if (!IronTeleportUnlocked && name == "$item_silver")
-      {
-        IronTeleportUnlocked = true;
-        Log.LogInfo("Unlocked iron teleporting.");
-        return true;
-      }
-
-      if (!SilverTeleportUnlocked && name == "$item_blackmetal")
-      {
-        SilverTeleportUnlocked = true;
-        Log.LogInfo("Unlocked silver teleporting.");
-        return true;
-      }
-
-      return false;
     }
   }
 }
---
+++ Tiers/Tier.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ForgivingPortals.Tiers
+{
+  public class Tier
+  {
+    public bool Unlocked { get; protected set; }
+
+    public readonly HashSet<string> Contains;
+    public readonly HashSet<string> UnlockedBy;
+    public readonly Tier PreviousTier;
+
+    public Tier(IEnumerable<string> contains, IEnumerable<string> unlockedBy, Tier previousTier = null) 
+      => (Contains, UnlockedBy, PreviousTier)
+        = (contains.ToHashSet(), unlockedBy.ToHashSet(), previousTier);
+
+    public void CheckUnlocks(IEnumerable<ItemDrop.ItemData> items)
+    {
+      foreach(var item in items)
+      {
+        CheckUnlock(item);
+      }
+    }
+
+    public void CheckUnlock(ItemDrop.ItemData item)
+    {
+      CheckUnlock(item.m_shared.m_name);
+    }
+
+    public void CheckUnlock(string item)
+    {
+      if (!Unlocked && UnlockedBy.Contains(item))
+      {
+        Unlock();
+      }
+    }
+
+    public void SyncItems(IEnumerable<ItemDrop.ItemData> items)
+    {
+      if (Unlocked)
+      {
+        foreach (var item in items)
+        {
+          if (Contains.Contains(item.m_shared.m_name))
+          {
+            item.m_shared.m_teleportable = true;
+          }
+        }
+      }
+
+      PreviousTier.SyncItems(items);
+    }
+
+    protected void Unlock()
+    {
+      Unlocked = true;
+
+      ForgivingPortalsManager.Log.LogInfo($"Unlocked tier containing items: {string.Join(",", Contains)}");
+
+      PreviousTier.Unlock();
+    }
+  }
+}
